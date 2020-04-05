3672033b53a558c58411caf0ed602ac94877056a
Lyuda.Pik <milasorokina96@gmail.com>
2020-04-06 01:50:57 +0300

    added rules

--- dist/data.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var statuses = [];
-var priorities = [];
-
-var task1 = new Task('1', 'Buy bread and eggs', '12.03.20');
-var task2 = new Task('2', 'Do homework');
-var task3 = new Task('3', 'Wash the dishes');
-var list = [task1, task2, task3];
